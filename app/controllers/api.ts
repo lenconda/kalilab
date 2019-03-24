426c5d3c205ce968641e606bd6b8d99fa64fa84d
lenconda <1053464288@qq.com>
2019-03-24 22:54:33 +0800

    🎉 app: initial commit

---
+++ app/controllers/api.ts
@@ -0,0 +1,23 @@
+import { JsonController, Get, Post, QueryParam, BodyParam } from 'routing-controllers'
+import APIService from '../services/api'
+import { Inject } from 'typedi'
+
+@JsonController('/')
+export default class APIController {
+
+  @Inject()
+  service: APIService
+
+  @Get('hello')
+  async hello (@QueryParam('name') name: string) {
+    const message = await this.service.getGreetingString(name)
+    return message
+  }
+
+  @Post('info')
+  async info (@BodyParam('optr1') optr1: number, @BodyParam('optr2') optr2: number) {
+    const result = await this.service.getArithmeticResult(optr1, optr2)
+    return result
+  }
+
+}
