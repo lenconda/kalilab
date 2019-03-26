7d86bfcb697f28d1a4eb7e9839ebe404aaa8b034
lenconda <1053464288@qq.com>
2019-03-26 17:10:05 +0800

    👍 feat: add MongoDB connection

---
+++ app/database/connection.ts
@@ -0,0 +1,54 @@
+import mongoose, { Mongoose } from 'mongoose'
+import { IMongoConfig } from '../../interfaces/config'
+import getLogger from '../utils/logger'
+import config from '../../config'
+const { DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT } = config
+
+const logger = getLogger(__filename)
+
+export default class MongoDBConnection {
+
+  /**
+   * @constructor
+   * @param {IMongoDBConfig} connectionParams
+   */
+  constructor (connectionParams: IMongoConfig) {
+    let {
+      DB_HOST,
+      DB_PORT,
+      DB_USER,
+      DB_PASSWORD,
+      DB_NAME } = connectionParams
+    let authorization = (DB_USER && DB_PASSWORD) ?
+      `${DB_USER}:${DB_PASSWORD}@` : ''
+    this.uri = `mongodb://${authorization}${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    this.database
+  }
+
+  private uri: string
+  private database: Promise<Mongoose | void>
+
+  /**
+   * connect to MongoDB server
+   * @public
+   */
+  public connect () {
+    this.database = mongoose
+      .connect(this.uri, {
+        useNewUrlParser: true, useCreateIndex: true })
+      .catch((err: Error) => logger.error(err.message))
+  }
+
+  /**
+   * disconnect from MongoDB server
+   * @public
+   */
+  public disconnect () {
+    mongoose.disconnect()
+  }
+
+}
