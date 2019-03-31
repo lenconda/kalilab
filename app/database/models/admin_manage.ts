7a016662943e96431a420080d37a264b5e44e9bf
lenconda <1053464288@qq.com>
2019-03-31 09:18:36 +0800

    🐛 fix: enable unique index for uuid in applications

--- app/database/models/admin_manage.ts
+++ app/database/models/admin_manage.ts
@@ -4,7 +4,7 @@ import { IApplication } from '../../../interfaces/admin_manage'
 interface IApplicationMongo extends IApplication, mongoose.Document {}
 
 const applicationSchema = new mongoose.Schema({
-  uuid: String,
+  uuid: { type: String, index: true, unique: true },
   binaryPath: String,
   name: String,
   avatar: String,
