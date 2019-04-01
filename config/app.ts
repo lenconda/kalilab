463f1402aa0cf83011c13e180a29a488683b932a
lenconda <1053464288@qq.com>
2019-04-01 15:29:15 +0800

    🐛 fix: fix log directory

--- config/app.ts
+++ config/app.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { IAppConfig } from '../interfaces/config'
 
 export const APP_LOG_DIR = process.env.APP_LOG_DIR ||
-  path.join(process.cwd(), './app/logs')
+  path.join(process.cwd(), './.app/logs')
 
 export const UUID_NAMESPACE = 'b4e19c21-f97f-489f-9aa7-37e108a50c6f'
 
