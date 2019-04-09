74db2ceb7f8fd8c7ee3ce16578c856e71ff24291
lenconda <1053464288@qq.com>
2019-04-09 10:21:50 +0800

    🐛 fix: entrypoint app.ts not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run init
-pm2-docker start --interpreter /app/node_modules/.bin/ts-node ./app/app.ts
+pm2-docker start --interpreter /app/node_modules/.bin/ts-node /app/app/app.ts
